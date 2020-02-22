6a865f4c3216522a3afb2c6e34807142f51552e9
JPalczewski <j.palczewski@gamedia.biz>
2020-02-22 14:38:45 +0100

    master - ApplicationConfiguration created to load app config variables

---
+++ src/main/scala/com/postdownloader/config/ApplicationConfiguration.scala
@@ -0,0 +1,35 @@
+package com.postdownloader.config
+import java.io.IOException
+
+import com.typesafe.config.ConfigException.{Missing, Null}
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
+
+import scala.util.{Failure, Success, Try}
+
+object ApplicationConfiguration extends LazyLogging {
+
+  private val DirectoryConfigPath: String = "application.directory.downloadPostsToDirectory"
+  lazy val PostWritingDirectory: String = Try {
+    ConfigFactory
+      .load()
+      .getString(DirectoryConfigPath)
+  } match {
+    case Success(path) => path
+    case Failure(exception: Null) =>
+      logger.error(
+        "Directory where posts should be saved to files cannot be null. Please provide valid path and try again."
+      )
+      throw ConfigurationLoadingException("Missing path to directory where posts should be saved to files.", exception)
+    case Failure(exception: Missing) =>
+      logger.error(
+        "Missing path to directory where posts should be saved to files. Please provide valid path and try again."
+      )
+      throw ConfigurationLoadingException("Missing path to directory where posts should be saved to files.", exception)
+    case Failure(exception) =>
+      logger.error("Something went wrong during configuration loading: ")
+      throw exception
+  }
+}
+
+private case class ConfigurationLoadingException(message: String, cause: Throwable) extends IOException(message, cause)
