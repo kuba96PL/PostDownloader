a2dd3fccb30a04f1d5ad4a4b98c0d1bdae1dd53f
JPalczewski <j.palczewski@gamedia.biz>
2020-02-23 18:30:40 +0100

    master - Http exception handling moved to JsonPlaceholderHttpClient.scala

--- src/main/scala/com/postdownloader/http/client/JsonPlaceholderHttpClient.scala
+++ src/main/scala/com/postdownloader/http/client/JsonPlaceholderHttpClient.scala
@@ -1,21 +1,27 @@
 package com.postdownloader.http.client
+import java.io.IOException
+
 import com.postdownloader.JSONProtocol.PostArrayJSONFormat
 import com.postdownloader.config.http.HttpConfiguration.JsonPlaceholderAPIBaseUrl
 import com.postdownloader.http.domain.Post
+import com.typesafe.scalalogging.LazyLogging
 import sttp.client._
 import sttp.client.sprayJson._
+class JsonPlaceholderHttpClient(implicit val httpBackend: SttpBackend[Identity, Nothing, NothingT])
+    extends LazyLogging {
 
-import scala.util.{Failure, Success, Try}
-class JsonPlaceholderHttpClient(implicit val httpBackend: SttpBackend[Identity, Nothing, NothingT]) {
-
-  def fetchAllPosts: Try[List[Post]] = {
+  def fetchAllPosts: List[Post] = {
     val fetchAllPostsRequest = basicRequest
       .get(uri"$JsonPlaceholderAPIBaseUrl/posts")
       .response(asJson[Array[Post]])
 
     fetchAllPostsRequest.send().body match {
-      case Left(error)                      => Failure(error)
-      case Right(responseBody: Array[Post]) => Success(responseBody.toList)
+      case Left(error) =>
+        logger.error("Failed to fetch posts")
+        throw PostFetchingException("An error occurred while trying to fetch posts", error)
+      case Right(responseBody: Array[Post]) => responseBody.toList
     }
   }
 }
+
+final private case class PostFetchingException(message: String, cause: Throwable) extends IOException(message, cause)
--- src/main/scala/com/postdownloader/service/PostService.scala
+++ src/main/scala/com/postdownloader/service/PostService.scala
@@ -1,22 +1,12 @@
 package com.postdownloader.service
 
-import java.io.IOException
-
 import com.postdownloader.http.client.JsonPlaceholderHttpClient
 import com.postdownloader.http.domain.Post
 import com.postdownloader.writer.PostFileWriter
-import com.typesafe.scalalogging.LazyLogging
 
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 class PostService(httpClient: JsonPlaceholderHttpClient, fileWriter: PostFileWriter) {
 
-  def fetchAndSave: List[Try[Post]] = fileWriter.writePosts(fetchPosts)
-  private def fetchPosts: List[Post] =
-    httpClient.fetchAllPosts match {
-      case Success(posts)     => posts
-      case Failure(exception) => throw PostFetchingException("An error occurred while fetching posts", exception)
-    }
+  def fetchAndSave: List[Try[Post]] = fileWriter.writePosts(httpClient.fetchAllPosts)
 }
-
-final case class PostFetchingException(message: String, cause: Throwable) extends IOException(message, cause)
